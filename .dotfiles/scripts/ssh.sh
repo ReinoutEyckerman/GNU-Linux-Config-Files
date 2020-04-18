517f6317f0a79cff3873b44e5f8720fb10e6542c
Vesyrak <reinouteyckerman@gmail.com>
2020-04-18 15:18:00 +0200

    Cleaned up legacy scripts and configs, renamed ansible, fixed ansible font download bug

--- .dotfiles/scripts/ssh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for f in ./scripts/core/*; do source $f; done
-
-function sshUbuntu() {
-  todo "Ssh setting is skipped in ubuntu"
-  todo "To re-enable this, you need to re-check existing configuration"
-}
-
-sshUbuntu
